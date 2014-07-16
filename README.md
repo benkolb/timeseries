39f1f2b19b20b18932ff4b449abd2aa7b457463d
Benedikt Kolb <benediktkolb86@gmail.com>
2014-07-16 13:56:04 +0200

    upload index and first files

---
+++ README.md
@@ -0,0 +1,4 @@
+timeseries
+==========
+
+Create and make available a set of widely used macroeconomic time series.
---
+++ get_series.r
@@ -0,0 +1,22 @@
+
+# Download time series from Eurostat
+# Benedikt Kolb, July 2014
+
+library(rdatamarket)
+library(xts)
+#library(xtsExtra)
+
+# Housekeeping
+wd = "C:/Users/Benedikt/Desktop/stuff/github/benkolb.github.io/timeseries"
+
+## from datamarket.com
+# GDP of Eurozone(27) (quarterly, nominal)
+gdpnq_raw <- dmseries("http://datamarket.com/data/set/18qt/gdp-and-main-components-current-prices#!ds=18qt!mh2=1:mh3=8:mh4=b:mh5=m")
+gdpnq <- as.xts(gdpnq_raw)
+names(gdpnq) <- "GDPnq"
+
+# Merge dataset and assemble csv file
+d <- merge(gdpnq)
+
+write.csv(d, paste(wd,"/series.csv", sep=""), quote=FALSE) # c("date","GDPnq")
+
